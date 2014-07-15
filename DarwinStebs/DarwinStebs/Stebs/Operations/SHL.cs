2be139948df827a9dc086fb23005092f3ef99250
Florian <florian@nexpose.ch>
2014-07-15 04:00:43 +0200

    implemented SHR and SHL commands

--- DarwinStebs/DarwinStebs/Stebs/Operations/SHL.cs
+++ DarwinStebs/DarwinStebs/Stebs/Operations/SHL.cs
@@ -2,10 +2,15 @@
 
 namespace DarwinStebs
 {
-	public class SHL
+	public class SHL : BaseOperation
 	{
-		public SHL ()
+		public SHL (CentralProcessingUnit cpu) : base(cpu)
 		{
+		}
+
+		public override void Execute (byte opcode, byte param1, byte param2)
+		{
+			cpu.GetRegister (param1).Value <<= 1;
 		}
 	}
 }
--- DarwinStebs/DarwinStebs/Stebs/Operations/SHR.cs
+++ DarwinStebs/DarwinStebs/Stebs/Operations/SHR.cs
@@ -2,10 +2,15 @@
 
 namespace DarwinStebs
 {
-	public class SHR
+	public class SHR: BaseOperation
 	{
-		public SHR ()
+		public SHR (CentralProcessingUnit cpu) : base(cpu)
 		{
+		}
+
+		public override void Execute (byte opcode, byte param1, byte param2)
+		{
+			cpu.GetRegister (param1).Value >>= 1;
 		}
 	}
 }
